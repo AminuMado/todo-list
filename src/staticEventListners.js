6bc27425ee817c4134fe0d1561ea4b888857808e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-11-15 20:38:59 +0100

    added project name update functionality

--- src/staticEventListners.js
+++ src/staticEventListners.js
@@ -1,22 +1,46 @@
+import createProject from "./projects";
+import todoList from ".";
+import render from "./render";
+
 const events = (() => {
+    const aside = document.getElementById('aside')
     const addProject = document.getElementById('add-project');
     const addProjectContainer = document.getElementById('add-project-container');
     const addProjectModal = document.getElementById('add-project-modal');
     const addProjectSubmitButton = document.getElementById('add-project-submit-button');
     const addProjectCancelButton = document.getElementById('add-project-cancel-button');
-
+    const addProjectTitle = document.getElementById('project-name-input');
+    const addProjectDescription = document.getElementById('project-description-input');
+    aside.addEventListener('click',(e) =>{
+        console.log(e.target)
+        
+        if(e.target.id == 'add-project-cancel-button'){
+            addProjectContainer.classList.remove('hide');
+            addProjectModal.classList.add('hide');
+        } else if(e.target.id == 'add-project-submit-button'){
+            let projectTitle = addProjectTitle.value;
+            let projectDescription = addProjectDescription.value;
+            if(projectTitle){
+                todoList.push(createProject(projectTitle,projectDescription))
+                console.log(todoList)
+                console.log(todoList[0].getTitle())
+                console.log(todoList[0].getDescription())
+                addProjectContainer.classList.remove('hide');
+                addProjectModal.classList.add('hide');
+                render.displayProject()
+                
+            }
+        } else if(e.target.classList == "project-item"){
+            render.displayProjectInfo(e.target.getAttribute("data-id"))
+        }
+        
+    })
+  
     addProject.addEventListener('click',()=>{
         addProjectContainer.classList.add('hide');
         addProjectModal.classList.remove('hide');
     })
-    addProjectCancelButton.addEventListener('click',()=>{
-        addProjectContainer.classList.remove('hide');
-        addProjectModal.classList.add('hide');
-    })
-    addProjectSubmitButton.addEventListener('click',()=>{
-        addProjectContainer.classList.remove('hide');
-        addProjectModal.classList.add('hide');
-    })
+  
 })()
 
 
